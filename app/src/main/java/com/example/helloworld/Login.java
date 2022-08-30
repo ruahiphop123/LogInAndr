cca39e1d4c1de251ad36de3e7bd78e480c9d732d
Duc <ricenguyen2310@gmail.com>
2022-08-30 11:12:16 +0700

    done

--- app/src/main/java/com/example/helloworld/Login.java
+++ app/src/main/java/com/example/helloworld/Login.java
@@ -8,22 +8,21 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button btback;
+public class Login extends AppCompatActivity {
+    Button btnext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
-        btback = (Button) findViewById(R.id.button);
-        btback.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login);
+        btnext = (Button) findViewById(R.id.button2);
+        btnext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mh1 = new Intent(MainActivity.this, Login.class);
-                startActivity(mh1);
+                Intent mh2 = new Intent(Login.this, MainActivity.class);
+                startActivity(mh2);
             }
         });
-
     }
 }
